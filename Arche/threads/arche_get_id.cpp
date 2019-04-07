11c53fd424720c8e9f56fb85b02ed44ed82e9ca7
Brandon Braun <brandonbraun653@gmail.com>
2019-04-07 16:42:07 -0500

    Implemented the Get, GetID, GetVer commands.
    #5

--- Arche/threads/arche_get_id.cpp
+++ Arche/threads/arche_get_id.cpp
@@ -9,21 +9,81 @@
  ********************************************************************************/
 
 /* Arche Includes */
+#include <Arche/arche.hpp>
 #include <Arche/threads.hpp>
+#include <Arche/commands.hpp>
 
 /* Chimera Includes */
 #include <Chimera/chimera.hpp>
 #include <Chimera/threading.hpp>
+#include <Chimera/system.hpp>
 
 namespace Arche
 {
   void threadGetID( void *argument )
   {
+    static constexpr uint8_t numBytes     = 2;
+    static constexpr uint32_t DMADelay_mS = 5;
+    
+    Chimera::System::Identifier system;
+    std::array<uint8_t, 3> getIDPayload{};
+
     Chimera::Threading::signalSetupComplete();
+
+    /*------------------------------------------------
+    Post-initialization, get the handle to the serial
+    root thread so we can wake it up later.
+    ------------------------------------------------*/
+    TaskHandle_t dispatchThread = xTaskGetHandle( threadName_commandDispatch );
 
     for ( ;; )
     {
-      Chimera::delayMilliseconds( 100 );
+      /*------------------------------------------------
+      Wait for another thread to wake us up to do work
+      ------------------------------------------------*/
+      if ( ulTaskNotifyTake( pdTRUE, portMAX_DELAY ) == TaskMessage::WAKEUP )
+      {
+        if ( serial.reserve( Chimera::Threading::TIMEOUT_DONT_WAIT ) == Chimera::CommonStatusCodes::OK )
+        {
+          /*------------------------------------------------
+          Protocol Step 1: Send ACK byte
+          ------------------------------------------------*/
+          Command::sendACKByte();
+          Chimera::delayMilliseconds( DMADelay_mS );
+
+          /*------------------------------------------------
+          Protocol Step 2: Send the number of bytes + device ID
+          ------------------------------------------------*/
+          uint16_t deviceID = static_cast<uint16_t>( system.deviceID() );
+          getIDPayload[ 0 ] = numBytes;
+          getIDPayload[ 1 ] = static_cast<uint8_t>( ( deviceID & 0xFF00 ) >> 8 );
+          getIDPayload[ 2 ] = static_cast<uint8_t>( ( deviceID & 0x00FF ) );
+
+          serial.write( getIDPayload.data(), getIDPayload.size() );
+          Chimera::delayMilliseconds( DMADelay_mS );
+
+          /*------------------------------------------------
+          Protocol Step 3: Send ACK byte
+          ------------------------------------------------*/
+          Command::sendACKByte();
+          Chimera::delayMilliseconds( DMADelay_mS );
+
+          /*------------------------------------------------
+          We are done with the command sequence. Go back to the dispatch thread.
+          ------------------------------------------------*/
+          serial.release();
+        }
+        else
+        {
+          // TODO: Throw an assert to be logged here
+        }
+
+        /*------------------------------------------------
+        Regardless of what happens, wake up the dispatch
+        thread so more commands can be handled.
+        ------------------------------------------------*/
+        xTaskNotify( dispatchThread, TaskMessage::WAKEUP, eSetValueWithOverwrite );
+      }
     }
   }
 }
--- Arche/threads/arche_get_ver.cpp
+++ Arche/threads/arche_get_ver.cpp
@@ -9,7 +9,9 @@
  ********************************************************************************/
 
 /* Arche Includes */
+#include <Arche/arche.hpp>
 #include <Arche/threads.hpp>
+#include <Arche/commands.hpp>
 
 /* Chimera Includes */
 #include <Chimera/chimera.hpp>
@@ -17,13 +19,75 @@
 
 namespace Arche
 {
+  /*------------------------------------------------
+  Figure 5: AN3155 Rev 9, pg. 11
+  ------------------------------------------------*/
+  static constexpr std::array<uint8_t, 3> getVerResponse = {
+    Arche::BOOTLOADER_VERSION,
+    0x00, /* Get Command */
+    0x00  /* Get Version and Read Protection Status */
+  };
+  
   void threadGetVer( void *argument )
   {
+    /*------------------------------------------------
+    The DMA hardware is usually too fast for the serial port and
+    will overload it. This small time buffer proved useful in
+    stabilizing the system.
+    ------------------------------------------------*/
+    static constexpr uint32_t DMADelay_mS = 5;
+
     Chimera::Threading::signalSetupComplete();
 
+    /*------------------------------------------------
+    Post-initialization, get the handle to the serial
+    root thread so we can wake it up later.
+    ------------------------------------------------*/
+    TaskHandle_t dispatchThread = xTaskGetHandle( threadName_commandDispatch );
+    
     for ( ;; )
     {
-      Chimera::delayMilliseconds( 100 );
+      /*------------------------------------------------
+      Wait for another thread to wake us up to do work
+      ------------------------------------------------*/
+      if ( ulTaskNotifyTake( pdTRUE, portMAX_DELAY ) == TaskMessage::WAKEUP )
+      {
+        if ( serial.reserve( Chimera::Threading::TIMEOUT_DONT_WAIT ) == Chimera::CommonStatusCodes::OK )
+        {
+          /*------------------------------------------------
+          Protocol Step 1: Send ACK byte
+          ------------------------------------------------*/
+          Command::sendACKByte();
+          Chimera::delayMilliseconds( DMADelay_mS );
+
+          /*------------------------------------------------
+          Protocol Step 2: Send the number of bytes + version + supported commands
+          ------------------------------------------------*/
+          serial.write( getVerResponse.data(), getVerResponse.size() );
+          Chimera::delayMilliseconds( DMADelay_mS );
+
+          /*------------------------------------------------
+          Protocol Step 3: Send ACK byte
+          ------------------------------------------------*/
+          Command::sendACKByte();
+          Chimera::delayMilliseconds( DMADelay_mS );
+
+          /*------------------------------------------------
+          We are done with the command sequence. Go back to the dispatch thread.
+          ------------------------------------------------*/
+          serial.release();
+        }
+        else
+        {
+          // TODO: Throw an assert to be logged here
+        }
+
+        /*------------------------------------------------
+        Regardless of what happens, wake up the dispatch
+        thread so more commands can be handled.
+        ------------------------------------------------*/
+        xTaskNotify( dispatchThread, TaskMessage::WAKEUP, eSetValueWithOverwrite );
+      }
     }
   }
 }
